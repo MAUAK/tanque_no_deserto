205ed4d8668472c9d8f456e48fe8b280ad46a1e5
Kauam <kauamsilva360@gmail.com>
2023-05-12 19:24:04 -0300

    update

--- Assets/Graphs/FollowPath.cs
+++ Assets/Graphs/FollowPath.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class FollowPath : MonoBehaviour
 {
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 1.0f;
-    float rotSpeed = 2.0f;
+    public Transform goal;
+    public float speed = 5.0f;
+    public float accuracy = 1.0f;
+    public float rotSpeed = 2.0f;
     public GameObject wpManager;
-    GameObject[] wps;
-    GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
+    public GameObject[] wps;
+    public GameObject currentNode;
+    public int currentWP = 0;
+    public Graph g;
 
     void Start()
     {
@@ -49,11 +49,13 @@ public class FollowPath : MonoBehaviour
             goal = g.getPathPoint(currentWP).transform;
             Vector3 lookAtGoal = new Vector3(goal.position.x,
             this.transform.position.y,
-            goal.position.z);
+            goal.position.z );
             Vector3 direction = lookAtGoal - this.transform.position;
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
             Quaternion.LookRotation(direction),
             Time.deltaTime * rotSpeed);
+
+            transform.position = Vector3.MoveTowards(transform.position, goal.position, speed * Time.deltaTime);
         }
     }
 }
